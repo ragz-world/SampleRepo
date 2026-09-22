e776c397f2a3bfff4302d5d48d16a050812c1ca3
ragen <ragen@RAGENDHU>
2026-09-22 12:36:23 +0530

    22/09/2026

---
+++ SampleMaven/src/main/java/aggregation/Aggregation2.java
@@ -0,0 +1,34 @@
+package aggregation;
+
+public class Aggregation2 {
+	
+	String city,state;
+	Aggregation1 ref;
+	
+	public Aggregation2(String city,String state,Aggregation1 ref)
+	
+	{
+		this.city = city;
+		this.state = state;
+		this.ref = ref;
+	}
+	
+	public void print()
+	{
+		System.out.println(ref.name);
+		System.out.println(ref.rollno);
+		System.out.println(ref.address);
+		System.out.println(city);
+		System.out.println(state);
+		
+	}
+
+	public static void main(String[] args) {
+		
+		Aggregation1 obj = new Aggregation1("Ragz",21,"xafsfd");
+		Aggregation2 obj1 =new Aggregation2("Pathanamthitta","Kerala",obj);
+		obj1.print();
+
+	}
+
+}
